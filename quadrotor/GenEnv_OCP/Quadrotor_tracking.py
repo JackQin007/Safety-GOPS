e775cb4aa9a184e38bfd67cf6ad28f210a20de0c
Jack007qst <qst23@mails.tsinghua.edu.cn>
2023-11-01 15:21:25 +0800

    Quad_1D

--- quadrotor/GenEnv_OCP/Quadrotor_tracking.py
+++ quadrotor/GenEnv_OCP/Quadrotor_tracking.py
@@ -10,39 +10,19 @@ from quadrotor.GenEnv_OCP.context.ref_traj import QuadContext
 
 class QuadTracking(Env):
     def __init__(
-        self,
-        *,
-        pre_horizon: int = 10,
-        dt: float = 0.1,
-        path_para: Optional[Dict[str, Dict]] = None,
-        u_para: Optional[Dict[str, Dict]] = None,
-        max_acc: float = 3.0,
-        max_steer: float = np.pi / 6,
+        self,       
         **kwargs,
     ):
         self.robot: Quadrotor = Quadrotor(
-            dt=dt,
-            max_acc=max_acc,
-            max_steer=max_steer,
+
         )
         self.context: QuadContext = QuadContext(
-            # pre_horizon=pre_horizon,
-            # dt=dt,
-            # path_param=path_para,
-            # speed_param=u_para,
+           
         )
-        
-        # self.state_space = spaces.Box(
-        #     low=np.array([-self.x_threshold, -np.finfo(np.float32).max]), 
-        #     high=np.array([self.x_threshold, np.finfo(np.float32).max]), 
-        #     dtype=np.float32)
         self.state_space = self.robot.state_space
         self.action_space = self.robot.action_space
         self.max_episode_steps = 200
         self.seed()
-
-
-
 
     def reset(
         self,
@@ -59,14 +39,6 @@ class QuadTracking(Env):
         return self.robot._get_done()
     
     def _get_info(self) -> dict:
-        # return {
-        #     **super()._get_info(),
-        #     "ref_points": self.context.state.reference.copy(),
-        #     "path_num": self.context.state.path_num,
-        #     "u_num": self.context.state.speed_num,
-        #     "ref_time": self.context.state.ref_time,
-        #     "ref": self.context.state.reference[0].copy(),
-        # }
         return self.robot._get_info()
 
     def render(self, mode="human"):
